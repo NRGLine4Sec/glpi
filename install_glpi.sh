5588414ccf8db81512745320b643a59ceb4cbd3a
NRGLine4Sec <mailforgithub.nrgline@yopmail.com>
2018-12-20 21:55:42 +0100

    Update install_glpi.sh

--- install_glpi.sh
+++ install_glpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Installation de nfsen en cour, veuillez patientez"
+echo "Installation de GLPI/FusionInventory en cour, veuillez patientez"
 glpi_version="9.3.2"
 apt-get update -qq && apt-get upgrade -y -qq
 apt-get install apache2 php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-imap php7.0-ldap php7.0-mbstring php7.0-gd php7.0-cli php7.0-apcu php7.0-xmlrpc php7.0-xml php7.0-curl mariadb-server mariadb-client php-cas ntp -y -qq
